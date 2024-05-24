8f5ae016082dc003a953eac8e4ab626f7d40ac03
Ali <109229417+tamnends@users.noreply.github.com>
2024-05-24 22:24:27 +0300

    + Move BG

--- code0.js
+++ code0.js
@@ -3,8 +3,6 @@ gdjs.Untitled_32sceneCode.GDHeroObjects1= [];
 gdjs.Untitled_32sceneCode.GDHeroObjects2= [];
 gdjs.Untitled_32sceneCode.GDPlatform1Objects1= [];
 gdjs.Untitled_32sceneCode.GDPlatform1Objects2= [];
-gdjs.Untitled_32sceneCode.GDBGObjects1= [];
-gdjs.Untitled_32sceneCode.GDBGObjects2= [];
 gdjs.Untitled_32sceneCode.GDScoreObjects1= [];
 gdjs.Untitled_32sceneCode.GDScoreObjects2= [];
 gdjs.Untitled_32sceneCode.GD_951059Objects1= [];
@@ -23,6 +21,10 @@ gdjs.Untitled_32sceneCode.GD_951048Objects1= [];
 gdjs.Untitled_32sceneCode.GD_951048Objects2= [];
 gdjs.Untitled_32sceneCode.GDPlatform2Objects1= [];
 gdjs.Untitled_32sceneCode.GDPlatform2Objects2= [];
+gdjs.Untitled_32sceneCode.GDBG2Objects1= [];
+gdjs.Untitled_32sceneCode.GDBG2Objects2= [];
+gdjs.Untitled_32sceneCode.GDBG1Objects1= [];
+gdjs.Untitled_32sceneCode.GDBG1Objects2= [];
 
 
 gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
@@ -47,10 +49,10 @@ let isConditionTrue_0 = false;
 gdjs.copyArray(runtimeScene.getObjects("Platform1"), gdjs.Untitled_32sceneCode.GDPlatform1Objects1);
 gdjs.copyArray(runtimeScene.getObjects("Platform2"), gdjs.Untitled_32sceneCode.GDPlatform2Objects1);
 {for(var i = 0, len = gdjs.Untitled_32sceneCode.GDPlatform1Objects1.length ;i < len;++i) {
-    gdjs.Untitled_32sceneCode.GDPlatform1Objects1[i].setX(gdjs.Untitled_32sceneCode.GDPlatform1Objects1[i].getX() + (-(5)));
+    gdjs.Untitled_32sceneCode.GDPlatform1Objects1[i].setX(gdjs.Untitled_32sceneCode.GDPlatform1Objects1[i].getX() + (-(4)));
 }
 }{for(var i = 0, len = gdjs.Untitled_32sceneCode.GDPlatform2Objects1.length ;i < len;++i) {
-    gdjs.Untitled_32sceneCode.GDPlatform2Objects1[i].setX(gdjs.Untitled_32sceneCode.GDPlatform2Objects1[i].getX() + (-(5)));
+    gdjs.Untitled_32sceneCode.GDPlatform2Objects1[i].setX(gdjs.Untitled_32sceneCode.GDPlatform2Objects1[i].getX() + (-(4)));
 }
 }}
 
@@ -107,6 +109,74 @@ gdjs.copyArray(runtimeScene.getObjects("Platform1"), gdjs.Untitled_32sceneCode.G
 }
 
 
+{
+
+
+let isConditionTrue_0 = false;
+{
+gdjs.copyArray(runtimeScene.getObjects("BG1"), gdjs.Untitled_32sceneCode.GDBG1Objects1);
+gdjs.copyArray(runtimeScene.getObjects("BG2"), gdjs.Untitled_32sceneCode.GDBG2Objects1);
+{for(var i = 0, len = gdjs.Untitled_32sceneCode.GDBG1Objects1.length ;i < len;++i) {
+    gdjs.Untitled_32sceneCode.GDBG1Objects1[i].setX(gdjs.Untitled_32sceneCode.GDBG1Objects1[i].getX() + (-(5)));
+}
+}{for(var i = 0, len = gdjs.Untitled_32sceneCode.GDBG2Objects1.length ;i < len;++i) {
+    gdjs.Untitled_32sceneCode.GDBG2Objects1[i].setX(gdjs.Untitled_32sceneCode.GDBG2Objects1[i].getX() + (-(5)));
+}
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("BG1"), gdjs.Untitled_32sceneCode.GDBG1Objects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+for (var i = 0, k = 0, l = gdjs.Untitled_32sceneCode.GDBG1Objects1.length;i<l;++i) {
+    if ( gdjs.Untitled_32sceneCode.GDBG1Objects1[i].getX() < -((gdjs.Untitled_32sceneCode.GDBG1Objects1[i].getWidth())) ) {
+        isConditionTrue_0 = true;
+        gdjs.Untitled_32sceneCode.GDBG1Objects1[k] = gdjs.Untitled_32sceneCode.GDBG1Objects1[i];
+        ++k;
+    }
+}
+gdjs.Untitled_32sceneCode.GDBG1Objects1.length = k;
+if (isConditionTrue_0) {
+/* Reuse gdjs.Untitled_32sceneCode.GDBG1Objects1 */
+gdjs.copyArray(runtimeScene.getObjects("BG2"), gdjs.Untitled_32sceneCode.GDBG2Objects1);
+{for(var i = 0, len = gdjs.Untitled_32sceneCode.GDBG1Objects1.length ;i < len;++i) {
+    gdjs.Untitled_32sceneCode.GDBG1Objects1[i].setX((( gdjs.Untitled_32sceneCode.GDBG2Objects1.length === 0 ) ? 0 :gdjs.Untitled_32sceneCode.GDBG2Objects1[0].getX()) + (( gdjs.Untitled_32sceneCode.GDBG2Objects1.length === 0 ) ? 0 :gdjs.Untitled_32sceneCode.GDBG2Objects1[0].getWidth()));
+}
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("BG2"), gdjs.Untitled_32sceneCode.GDBG2Objects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+for (var i = 0, k = 0, l = gdjs.Untitled_32sceneCode.GDBG2Objects1.length;i<l;++i) {
+    if ( gdjs.Untitled_32sceneCode.GDBG2Objects1[i].getX() < -((gdjs.Untitled_32sceneCode.GDBG2Objects1[i].getWidth())) ) {
+        isConditionTrue_0 = true;
+        gdjs.Untitled_32sceneCode.GDBG2Objects1[k] = gdjs.Untitled_32sceneCode.GDBG2Objects1[i];
+        ++k;
+    }
+}
+gdjs.Untitled_32sceneCode.GDBG2Objects1.length = k;
+if (isConditionTrue_0) {
+gdjs.copyArray(runtimeScene.getObjects("BG1"), gdjs.Untitled_32sceneCode.GDBG1Objects1);
+/* Reuse gdjs.Untitled_32sceneCode.GDBG2Objects1 */
+{for(var i = 0, len = gdjs.Untitled_32sceneCode.GDBG2Objects1.length ;i < len;++i) {
+    gdjs.Untitled_32sceneCode.GDBG2Objects1[i].setX((( gdjs.Untitled_32sceneCode.GDBG1Objects1.length === 0 ) ? 0 :gdjs.Untitled_32sceneCode.GDBG1Objects1[0].getX()) + (( gdjs.Untitled_32sceneCode.GDBG1Objects1.length === 0 ) ? 0 :gdjs.Untitled_32sceneCode.GDBG1Objects1[0].getWidth()));
+}
+}}
+
+}
+
+
 };
 
 gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
@@ -116,8 +186,6 @@ gdjs.Untitled_32sceneCode.GDHeroObjects1.length = 0;
 gdjs.Untitled_32sceneCode.GDHeroObjects2.length = 0;
 gdjs.Untitled_32sceneCode.GDPlatform1Objects1.length = 0;
 gdjs.Untitled_32sceneCode.GDPlatform1Objects2.length = 0;
-gdjs.Untitled_32sceneCode.GDBGObjects1.length = 0;
-gdjs.Untitled_32sceneCode.GDBGObjects2.length = 0;
 gdjs.Untitled_32sceneCode.GDScoreObjects1.length = 0;
 gdjs.Untitled_32sceneCode.GDScoreObjects2.length = 0;
 gdjs.Untitled_32sceneCode.GD_951059Objects1.length = 0;
@@ -136,6 +204,10 @@ gdjs.Untitled_32sceneCode.GD_951048Objects1.length = 0;
 gdjs.Untitled_32sceneCode.GD_951048Objects2.length = 0;
 gdjs.Untitled_32sceneCode.GDPlatform2Objects1.length = 0;
 gdjs.Untitled_32sceneCode.GDPlatform2Objects2.length = 0;
+gdjs.Untitled_32sceneCode.GDBG2Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDBG2Objects2.length = 0;
+gdjs.Untitled_32sceneCode.GDBG1Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDBG1Objects2.length = 0;
 
 gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
 
